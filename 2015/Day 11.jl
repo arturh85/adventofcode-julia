1bd28efda858119b623c47ee56b7c3d6eefe2cc7
arturh85 <arturh@arturh.de>
2021-05-13 23:29:08 +0000

    Deploying to gh-pages from  @ b78f4e602a44920d166c8896f6514b65bbab6c81 🚀

--- 2015/Day 11.jl
+++ 2015/Day 11.jl
@@ -66,6 +66,92 @@ end
 # ╔═╡ 58f76b47-542f-438b-ac71-694a76d9d77b
 @assert next("ya") == "yb"
 
+# ╔═╡ 9a03d3c5-3bc6-48c7-87cc-49ef6cbd31d4
+function has_straight(s) 
+	for idx in 1:length(s)-2
+		a = Int(s[idx])
+		b = Int(s[idx+1])
+		c = Int(s[idx+2])
+		
+		if b == a + 1 && c == b + 1
+			return true
+		end
+	end
+	false
+end
+
+# ╔═╡ b10b13e9-de70-4c09-8b7a-9cd530d1e3ac
+@assert has_straight("hijklmmn")
+
+# ╔═╡ 2d9e27ff-5557-4c7e-b6a8-b3accd15f7f0
+@assert !has_straight("abbceffg")
+
+# ╔═╡ 26342478-769e-4ef1-a3f3-ddf5aad2e281
+has_no_confusing(s) = !occursin("i", s) && !occursin("o", s) && !occursin("l", s)
+
+# ╔═╡ a470ee13-e98c-423b-8398-e6c8774983c5
+@assert !has_no_confusing("hijklmmn")
+
+# ╔═╡ dd4c2b0b-d8af-411f-9290-d36398772267
+@assert has_no_confusing("abbceffg")
+
+# ╔═╡ a0681275-076f-400f-8862-74c0cba5ffc1
+# Passwords must contain at least two different, non-overlapping pairs of letters, like aa, bb, or zz.
+
+# ╔═╡ 2144ecf3-79c2-4de1-8aa4-52b4e6b57208
+function has_two_nonoverlapping_pair(s) 
+	for idx1 in 1:length(s)-1
+		if s[idx1] == s[idx1+1]
+			for idx2 in idx1+2:length(s)-1
+				if s[idx2] == s[idx2+1]
+					return true
+				end
+			end
+		end
+	end
+	false
+end
+
+# ╔═╡ d103a1b1-7ade-41da-bc1e-b01337333675
+@assert has_two_nonoverlapping_pair("abbceffg")
+
+# ╔═╡ 5a13d4d9-7451-4fb0-8aaf-8d47947c10e4
+check1(s) = has_straight(s) && has_no_confusing(s) && has_two_nonoverlapping_pair(s)
+
+# ╔═╡ de4134e2-cc3f-4872-9244-4596d8525292
+function next_valid(s) 
+	while true
+		s = next(s)
+		if check1(s)
+			return s
+		end
+	end
+end
+
+# ╔═╡ 6b671ffc-ccc1-4b84-9ddb-7f7137995348
+@assert next_valid("abcdefgh") == "abcdffaa"
+
+# ╔═╡ a33d8ddb-5ef5-4c0c-891c-eef1368fbcc9
+@assert next_valid("ghijklmn") == "ghjaabcc"
+
+# ╔═╡ cbcd58ec-5f81-4ca4-8a4f-2a570db79f2a
+part1 = next_valid(puzzle_input)
+
+# ╔═╡ bd64f7b1-4074-4eeb-925d-eb8cc7a524bf
+md"Your puzzle answer was `hxbxxyzz`."
+
+# ╔═╡ aa05c55c-e79d-4846-95e1-823f0a5ea2e3
+md"""
+# Part Two
+Santa's password expired again. **What's the next one?**
+"""
+
+# ╔═╡ 1ef77364-44a3-4819-b0c3-11cca842e53c
+part2 = next_valid("hxbxxyzz")
+
+# ╔═╡ bd67cd4e-af24-403c-a3ba-9753f11dc21c
+md"Your puzzle answer was `hxcaabcc`."
+
 # ╔═╡ Cell order:
 # ╟─e89c666e-b0e7-11eb-22cf-7f6fd1e299bb
 # ╠═ec540096-d9d1-4b5f-b6a2-16c470322424
@@ -74,3 +160,21 @@ end
 # ╠═c4d6ad5b-f500-4435-a8eb-76c1f33f9775
 # ╠═8f2717d2-ed5e-4e4e-abcd-6ff2982d8cd6
 # ╠═58f76b47-542f-438b-ac71-694a76d9d77b
+# ╠═9a03d3c5-3bc6-48c7-87cc-49ef6cbd31d4
+# ╠═b10b13e9-de70-4c09-8b7a-9cd530d1e3ac
+# ╠═2d9e27ff-5557-4c7e-b6a8-b3accd15f7f0
+# ╠═26342478-769e-4ef1-a3f3-ddf5aad2e281
+# ╠═a470ee13-e98c-423b-8398-e6c8774983c5
+# ╠═dd4c2b0b-d8af-411f-9290-d36398772267
+# ╠═a0681275-076f-400f-8862-74c0cba5ffc1
+# ╠═2144ecf3-79c2-4de1-8aa4-52b4e6b57208
+# ╠═d103a1b1-7ade-41da-bc1e-b01337333675
+# ╠═5a13d4d9-7451-4fb0-8aaf-8d47947c10e4
+# ╠═de4134e2-cc3f-4872-9244-4596d8525292
+# ╠═6b671ffc-ccc1-4b84-9ddb-7f7137995348
+# ╠═a33d8ddb-5ef5-4c0c-891c-eef1368fbcc9
+# ╠═cbcd58ec-5f81-4ca4-8a4f-2a570db79f2a
+# ╟─bd64f7b1-4074-4eeb-925d-eb8cc7a524bf
+# ╟─aa05c55c-e79d-4846-95e1-823f0a5ea2e3
+# ╠═1ef77364-44a3-4819-b0c3-11cca842e53c
+# ╟─bd67cd4e-af24-403c-a3ba-9753f11dc21c
